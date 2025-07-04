5e6509d7fa1e46fa98594e3a7e71a3703ce407c9
sagar_patil <sagarpatil35137@gmail.com>
2025-07-04 23:04:37 +0530

    Minor changes

--- src/modules/my-proposal/proposal.service.ts
+++ src/modules/my-proposal/proposal.service.ts
@@ -8,15 +8,15 @@ export class ProposalService {
 
     async getResponse(dto: ProposalDto) {
         const yesTemp = `
-        <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 10px; text-align: center;">
+        <div style="max-width: 600px; margin: auto; background-color: #fbfbfb; padding: 20px; border-radius: 10px; text-align: center;">
     <h1 style="color: #f472b6;">${dto.hers_name} Said YES! 🎉</h1>
     <p style="font-size: 18px; color: #333333;">Congratulations! ${dto.hers_name} just gave you the best answer in the world.</p>
     <p style="font-size: 16px; color: #666666;">This is the beginning of something beautiful. Cherish every moment together. 💖</p>
 </div>
 `
         const noTemp = `
-<div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 10px; text-align: center;">
-    <h1 style="color: #f44336;">It's a No from ${dto.hers_name}... 💔</h1>
+<div style="max-width: 600px; margin: auto; background-color: #fbfbfb; padding: 20px; border-radius: 10px; text-align: center;">
+    <h1 style="color:rgb(226, 70, 59);">It's a No from ${dto.hers_name}... 💔</h1>
     <p style="font-size: 18px; color: #333333;">${dto.reason}</p>
     <p style="font-size: 16px; color: #666666;">But don't lose hope! Sometimes hearts need more time to connect.</p>
 </div>
